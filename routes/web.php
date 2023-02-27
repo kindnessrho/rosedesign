8580a9ab07156d35e5cf901531bc4882a513e890
kindness Rho <dev@coderholics.com>
2023-02-27 16:03:02 +0100

    get method
    fix get method error

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::match(['get', 'head'], '/', function () {
     return view('welcome');
 });
